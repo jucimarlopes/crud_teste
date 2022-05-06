0aec988fcc93b47ea1cea4375077649bb0db16d4
jucimarlopes <personalsupport@hotmail.com>
2022-05-06 00:19:34 -0300

    06/05/2022 00:19

---
+++ crud_teste.php
@@ -0,0 +1,52 @@
+<?php
+
+class Crud_Teste
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        
+    }
+    public function cadastrarContainer ()
+    {
+        
+    }
+    public function cadastrarMovimento ()
+    {
+        
+    }
+    public function listarContainers()
+    {
+        
+    }
+    public function listarMovimentos()
+    {
+        
+    }
+    public function listarContainer()
+    {
+        
+    }
+    public function listarMovimento()
+    {
+        
+    }
+    public function atualizarContainer ()
+    {
+        
+    }
+    public function atualizarMovimento ()
+    {
+        
+    }
+    public function deletarContainer ()
+    {
+        
+    }
+    public function deletarMov ()
+    {
+        
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once 'crud_teste.php';
+$crud_teste = new Crud_Teste();
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+
+    <!-- CSS Customizado -->
+    <link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />  
+    
+
+    <title></title>
+</head>
+
+<body>
+ 
+</body>
+
+</html>
